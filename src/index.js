bfe3ce3611335055e3cdc7e63979b158ad3fc1fa
JotaVPinheiro <jotavpinheiro@gmail.com>
2022-06-13 12:49:17 -0300

    player can choose where to mark on the board

--- src/index.js
+++ src/index.js
@@ -1,18 +1,52 @@
 const colors = require("ansi-colors");
 
 const board = ["", "", "", "", "", "", "", "", ""];
+const moves = [];
 
 const printBoard = (board) => {
   for (let i = 0; i < 3; i++) {
-    for (let j = 0; j < 3; j++) {
-      if (i < 2)
-        process.stdout.write(colors.underline(board[i * 4 + j] || " "));
-      else process.stdout.write(board[i * 4 + j] || " ");
+    for (let j = i * 3; j < i * 3 + 3; j++) {
+      if (i < 2) process.stdout.write(colors.underline(board[j] || " "));
+      else process.stdout.write(board[j] || " ");
 
-      if (j < 2) process.stdout.write("|");
+      if (j % 3 !== 2) process.stdout.write("|");
     }
     process.stdout.write("\n");
   }
 };
 
+const askMove = () => process.stdout.write("Escolha uma posição: ");
+
+const endGame = () => {
+  console.log("");
+  printBoard(board);
+  console.log("Fim de jogo!");
+};
+
+process.stdin.on("data", (data) => {
+  const move = Number(data);
+
+  if (move === NaN || move < 0 || move > 8) {
+    console.log(colors.bold("\nMovimento inválido!\n"));
+    printBoard(board);
+    askMove();
+    return;
+  }
+
+  moves.push(move);
+  board[move] = "X";
+
+  if (moves.length < board.length) {
+    console.log("");
+    printBoard(board);
+    askMove();
+    return;
+  }
+
+  process.exit();
+});
+
+process.on("exit", endGame);
+
 printBoard(board);
+askMove();
