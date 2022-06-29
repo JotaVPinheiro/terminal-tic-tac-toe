c46e4bef1c61fd70fa0a2747688d05916ec36481
JotaVPinheiro <jotavpinheiro@gmail.com>
2022-06-29 20:05:56 -0300

    COM does a fixed first move

--- src/COMPlay.js
+++ src/COMPlay.js
@@ -1,5 +1,21 @@
+const corners = [0, 2, 6, 8];
+const center = 4;
+
+const firstMove = () => {
+  return corners[Math.floor(Math.random() * 4)];
+};
+
+const secondMove = (playerMoves) => {
+  if (playerMoves.includes(center))
+    return corners[Math.floor(Math.random() * 4)];
+
+  return center;
+};
+
 const COMPlay = (playerMoves, comMoves) => {
   if (playerMoves.length + comMoves.length >= 9) return null;
+  if (playerMoves.length + comMoves.length == 0) return firstMove();
+  if (playerMoves.length + comMoves.length == 1) return secondMove(playerMoves);
 
   while (true) {
     const move = Math.floor(Math.random() * 9);
